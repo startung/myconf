b96d22332e7027965fae09e561db795ac99d1f35
startung <717612+startung@users.noreply.github.com>
2018-08-02 14:22:08 +0100

    Added keyboard bindings and script for openrazor

---
+++ script/keyboard.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+path="/sys/bus/hid/drivers/razerkbd/0003:1532:021E.0005"
+#state=$(xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$")
+
+case $1 in
+  "breath")
+    echo -n "1" > $path/matrix_effect_breath
+    ;;
+  "off")
+    echo -n "1" > $path/matrix_effect_none
+    ;;
+  "spectrum")
+    echo -n "1" > $path/matrix_effect_spectrum
+    ;;
+  "white")
+    echo -n -e "\xFF\xFF\xFF"  > $path/matrix_effect_static
+    ;;
+  "red")
+    echo -n -e "\xFF\x00\x00"  > $path/matrix_effect_static
+    ;;
+  "green")
+    echo -n -e "\x00\xFF\x00"  > $path/matrix_effect_static
+    ;;
+  "blue")
+    echo -n -e "\x00\x00\xFF"  > $path/matrix_effect_static
+    ;;
+  "reactive")
+    echo -n -e "\x02\xFF\xFF\xFF"  > $path/matrix_effect_reactive
+    ;;
+  "full")
+    echo -n "255"  > $path/matrix_brightness
+    ;;
+  "dim")
+    echo -n "127"  > $path/matrix_brightness
+    ;;
+
+esac
+
